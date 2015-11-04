81c0db80e20744a6d4f061a2810df9e61a021ccb
HOON@I/O <segoist43@gmail.com>
2015-11-04 21:34:40 +0900

    form initialization

--- MemoPad/Form1.vb
+++ MemoPad/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class frmMain
-
+    Dim argFilename As String '파일명을 보관하는 변수를 생성합니다.
+    Dim strModify As Boolean = False 'rtxMemo 의 텍스트 변경 여부를 확인하는 변수를 생성합니다.
+    Dim Fileopen As Boolean = False '파일의 Open 여부를 확인하는 변수를 생성합니다.
 End Class
